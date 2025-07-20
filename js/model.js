ec82f4733782c91ebbe8bcc55081524519214366
madoumadev <almega95@gmail.com>
2025-07-20 23:45:11 +0300

    add download btn

--- js/model.js
+++ js/model.js
@@ -1,5 +1,7 @@
 import {CvBuilder} from "./classes/cv_blocks.js";
+import {DownloadButtonBlock} from "./classes/download_block.js";
 
 export const model = [
     new CvBuilder(),
+    new DownloadButtonBlock()
 ];
